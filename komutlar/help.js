d972e532aed27482349a6d3c1560b8ae4f03f528
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-15 21:47:42 +0300

    Rename yardım.js to help.js

---
+++ komutlar/help.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message, args) {
+    let type = args.slice(0).join(' ');
+    if (type.length < 1) return message.channel.send(
+new Discord.RichEmbed()
+.setDescription('Kullanım: f!yardım <-yardım istediğiniz konu>'));
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setDescription('Yardım istediğiniz konu yetkililere ulaştı!')
+message.channel.send(embed)
+const embed2 = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setDescription(`**${message.author.tag}** adlı kullanıcı yardım istiyor:`)
+.addField(`Kulanıcı Bilgileri`, `Kullanıcı ID: ${message.author.id}\nKullanıcı Adı: ${message.author.username}\nKullanıcı Tagı: ${message.author.discriminator}`)
+.addField("yardım", type)
+.setThumbnail(message.author.avatarURL)
+client.channels.get('577122905263964189').send(embed2);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'öneri',
+  description: 'Yetkililerden yardım istersiniz',
+  usage: 'yetkili yardım <Yardım istediğiniz konu>'
+};
