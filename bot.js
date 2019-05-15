16eac35886de02f97a063710674823e4b9439661
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-15 15:18:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,6 +82,8 @@ client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
 		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
 			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
+		} else {
+		msg.reply('Aleyküm selam, hoş geldin ^^');
 		}
 	}
 });
@@ -106,4 +108,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(process.env.Bot_TOKEN);
+client.login(ayarlar.token);
