a33b9f6193d5c6e4d96fe6737ea45797ebad1c0f
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-16 22:32:27 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('https://media.giphy.com/media/s6EYTqTRqujIY/giphy.gif AAAAAAA **' + username + ' Discord Sunucuma Hoş Geldiiiiiinnnnn**');
+    member.sendMessage('https://media.giphy.com/media/s6EYTqTRqujIY/giphy.gif AAAAAAA **' + username + ' Discord Sunucuma Hoş Geldiiiiiinnnnnnnn**');
 
 };
