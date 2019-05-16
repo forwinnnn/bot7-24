d0f932ffb8ed1315071d3d2f35cb6d5e31d1f215
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-16 22:23:00 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('http://www.huffingtonpost.es/2014/12/16/lecciones-los-simpsons-_n_6333650.html?utm_hp_ref=spain&ncid=tweetlnkushpmg00000067 AAAAAAA **' + username + 'Discord Sunucuma Hoş Geldiiiiiinnnnn**');
+    member.sendMessage('https://tr.pinterest.com/pin/343258802826121320/7 AAAAAAA **' + username + ' Discord Sunucuma Hoş Geldiiiiiinnnnn**');
 
 };
