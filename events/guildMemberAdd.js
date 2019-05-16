7f8133d3d15c6f721ed85178991e272dbe3336bf
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-16 22:18:07 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send('hg '+username+'');
+    member.sendMessage('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9ibW44aDiAhXHaVAKHTWUBrwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F343258802826121320%2F&psig=AOvVaw2xOQKe4Rz_FRglDmxY2WBI&ust=1558118614572156 AAAAAAA **' + username + 'Discord Sunucuma Hoş Geldiiiiiinnnnn**');
+
 };
