e6fad8a03a23c2df96e03642b95df1ecd4a9fc2c
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-16 22:21:00 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9ibW44aDiAhXHaVAKHTWUBrwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F343258802826121320%2F&psig=AOvVaw2xOQKe4Rz_FRglDmxY2WBI&ust=1558118614572156 AAAAAAA **' + username + 'Discord Sunucuma Hoş Geldiiiiiinnnnn**');
+    member.sendMessage('http://www.huffingtonpost.es/2014/12/16/lecciones-los-simpsons-_n_6333650.html?utm_hp_ref=spain&ncid=tweetlnkushpmg00000067 AAAAAAA **' + username + 'Discord Sunucuma Hoş Geldiiiiiinnnnn**');
 
 };
