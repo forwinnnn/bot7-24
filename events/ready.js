dab127a7d80fe52d63c2446597512fa7ace1eeed
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-18 12:21:34 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,28 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
+var oyun = [
+        "t!yardım - Otorol Güncellendi🔥",
+        "t!yardım - Küfür Engel✨",
+        "t!yardım - Reklam Engel⭐️",
+        "t!yardım - +hoşgeldin-ayarla Güncellendi💥",
+        "t!yardım - +slowmode 🌹",
+        "t!yardım - Beni Sunucuna Ekle⚡️",
+        "t!yardım - +sunucunutanıt YENİ🍂 ",
+        "t!yardım - +talep YENİ📫 ",
+        "t!yardım - +düello Yeni 🤛",
+        "t!yardım - +sayaç Yeni 👑",
+        "t!kayıtlar-ayarla - Yeni"
+  
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/For_Win_Bot" );
+        }, 2 * 2500);
+    
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
