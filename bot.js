2e726fdd121ac7a5f3c4b1b1c02fcc8db83104eb
forwinnnn <50663435+forwinnnn@users.noreply.github.com>
2019-05-20 00:02:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,6 +83,26 @@ client.on('message', msg => {
     msg.channel.sendMessage('Aleyküm Selam Hoş Geldin');
   }
 });
+let linkEngel = JSON.parse(fs.readFileSync("././jsonlar/linkEngelle.json", "utf8"));
+client.on("message", msg => { 
+if (!linkEngel[msg.guild.id]) return;
+if (linkEngel[msg.guild.id].linkEngel === "kapali") return;
+    if (linkEngel[msg.guild.id].linkEngel === "acik") {
+    var regex = new RegExp(/(discord.gg|http|.gg|.com|.net|.org|invite|İnstagram|Facebook|watch|Youtube|youtube|facebook|instagram)/)
+    if (regex.test(msg.content)== true) {
+    if (!msg.member.hasPermission("ADMINISTRATOR")) {
+      msg.delete()
+       msg.channel.send(`<@${msg.author.id}>`).then(message => message.delete(5000));
+        var e = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor("Link Engeli!")
+        .setDescription(`Bu sunucuda linkler **${client.user.username}** tarafından engellenmektedir! Link atmana izin vermeyeceğim!`)
+        msg.channel.send(e).then(message => message.delete(5000));
+    }
+}
+    }
+});
+
 client.elevation = message => {
   if(!message.guild) {
 	return; }
